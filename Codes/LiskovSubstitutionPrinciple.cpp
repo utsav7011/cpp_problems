02d32c43b11649a470f4dd8dd7659412f6e34367
--list <utsavjain@utsavs-MacBook-Air.local>
2026-03-31 22:14:00 +0530

    SOLID Principles

--- Codes/LiskovSubstitutionPrinciple.cpp
+++ Codes/LiskovSubstitutionPrinciple.cpp
@@ -1,20 +1,112 @@
 /**
- * 
+ *
  * subclasses should be substitutable for threit base classes.
- * 
+ *
  * A base class
  * B sub-class
- * 
- * say client which is expecting the base class referrence and we padss the subclass referrence then there should be no breaking changes. 
+ *
+ * say client which is expecting the base class referrence and we padss the subclass referrence then there should be no breaking changes.
  * Subclass should be simiarly substitutable and should be able to take place of the base class
+ *
+ * when can we overwrite the parent class with child class: 
  * 
+ * Broader class also called as Parent class
+ * narrow Class: Child Class
  * 
+ * Signature Rule: 
+ * Property Rule: 
+ * Method Rule: 
+ *
  */
 
-
-#include<iostream>
+#include <iostream>
 using namespace std;
+class NonWithdrawableAccount
+{
+  virtual void deposit(double amount) = 0;
+};
+class WithdrawableAccount
+{
+  virtual void withdraw(double amount) = 0;
+};
+class FixedDeposit : public NonWithdrawableAccount
+{
+private:
+  double balance;
 
-int main () { 
+public:
+  FixedDeposit()
+  {
+    balance = 0;
+  }
+  void deposit(double amount)
+  {
+    balance += amount;
+    cout << endl
+         << "bhalance is :::::" << balance;
+  }
+};
+class SavingsAccount : public WithdrawableAccount
+{
+private:
+  double balance;
 
+public:
+  SavingsAccount()
+  {
+    balance = 0;
+  }
+  void deposit(double amount)
+  {
+    balance += amount;
+    cout << endl
+         << "Deposited the amopount::::" << balance;
+  }
+
+  void withdrawl(double amount)
+  {
+    if (amount <= balance)
+    {
+      balance -= amount;
+    }
+    else
+    {
+      cout << endl
+           << "Balance is too looo !!!!!" << endl;
+    }
+  }
+};
+class CurrentAccount : public WithdrawableAccount
+{
+private:
+  double balance;
+
+public:
+  CurrentAccount()
+  {
+    balance = 0;
+  }
+  void deposit(double amount)
+  {
+    balance += amount;
+    cout << endl
+         << "Deposited the amopount::::" << balance;
+  }
+
+  void withdrawl(double amount)
+  {
+    if (amount <= balance)
+    {
+      balance -= amount;
+    }
+    else
+    {
+      cout << endl
+           << "Balance is too looo !!!!!" << endl;
+    }
+  }
+};
+
+int main()
+{
 }
